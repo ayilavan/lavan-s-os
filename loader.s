57709843e283e15a4f73954b0f97db276ff6178e
ayilavan <ajeelavan1503@gmail.com>
2021-07-20 17:35:04 +0530

    Call C function in assembly language

--- loader.s
+++ loader.s
@@ -21,5 +21,12 @@ global loader                   ; the entry symbol for ELF
         mov eax, 0xCAFEBABE         ; place the number 0xCAFEBABE in the register eax
         mov esp, kernel_stack + KERNEL_STACK_SIZE   ; point esp to the start of the
                                                 ; stack (end of memory area)
+                                                ; The assembly code
+    extern sum_of_three   ; the function sum_of_three is defined elsewhere
+
+    push dword 3            ; arg3
+    push dword 2            ; arg2
+    push dword 1            ; arg1
+    call sum_of_three       ; call the function, the result will be in eax
     .loop:
         jmp .loop                   ; loop forever

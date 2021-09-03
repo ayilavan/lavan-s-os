7b25da3c173de5be30e526122b457ab286f61f67
Pusparasa Ayilavan <63444297+ayilavan@users.noreply.github.com>
2021-09-03 12:13:45 +0530

    integrate segmentation (#4)

--- kmain.c
+++ kmain.c
@@ -1,13 +1,17 @@
 #include "drivers/frame_buffer.h"
 #include "drivers/serial_port.h"
+#include "segments/memory_segments.h"
 
 /* The C function */
 int sum_of_three(int arg1, int arg2, int arg3)
 {
-char text[] = "wel come";
 
-fb_write(text, 19);
-serial_write(0x3F8, text, 19);
+segments_install_gdt();
+char buffer[] = "wel come";
+
+fb_write(buffer, 19);
+serial_write(0x3F8, buffer, 19);
+
 
 return arg1 + arg2 + arg3;
 }

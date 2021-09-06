47af07284c11c44e1fb18d8423b4b55a02ddc399
Pusparasa Ayilavan <63444297+ayilavan@users.noreply.github.com>
2021-09-06 20:26:02 +0530

    Add files via upload

---
+++ drivers/serial_port/serial_port.h
@@ -0,0 +1,21 @@
+#ifndef _SERIAL_H_
+#define _SERIAL_H_
+
+#define SERIAL_COM1_BASE                0x3F8
+
+#define SERIAL_DATA_PORT(base)          (base)
+#define SERIAL_FIFO_COMMAND_PORT(base)  (base + 2)
+#define SERIAL_LINE_COMMAND_PORT(base)  (base + 3)
+#define SERIAL_MODEM_COMMAND_PORT(base) (base + 4)
+#define SERIAL_LINE_STATUS_PORT(base)   (base + 5)
+
+#define SERIAL_LINE_ENABLE_DLAB         0x80
+
+void serial_configure_baud_rate(unsigned short com, unsigned short divisor);
+void serial_configure_line(unsigned short com);
+
+int serial_is_transmit_fifo_empty(unsigned int com);
+
+void serial_write(char *buf, unsigned int len);
+
+#endif
--- kmain.c
+++ kmain.c
@@ -1,30 +1,32 @@
-    #include "drivers/frame_buffer.h"
-    #include "drivers/serial_port.h"
-    #include "drivers/io.h"
-    #include "segments/segments.h"
-    #include "interrupts/interrupts.h"
-    #include "interrupts/keyboard.h"
-    #include "interrupts/pic.h"
-    #include "multiboot.h"
+ #include "drivers/serial_port/serial_port.h"
+#include "memory/segmentation/segments.h"
+#include "drivers/interrupts/interrupts.h"
+#include "multiboot.h"
+#include "memory/paging/paging.h"
     
+    void init(){
+   segments_install_gdt();
+   interrupts_install_idt();
+   init_paging();
+}
 
    
     int kmain(unsigned int ebx)
     {
+    init();
 
        //char arr[] = "Wel come";
        //fb_move_cursor(6*80);
        //fb_write(arr, 20);
        //serial_write(arr, 20);
-       segments_install_gdt();
-       interrupts_install_idt();
+       
        
        multiboot_info_t *mbinfo = (multiboot_info_t *) ebx;
 multiboot_module_t* modules = (multiboot_module_t*) mbinfo->mods_addr;
 unsigned int address_of_module = modules->mod_start;
 
 if((mbinfo->mods_count) == 1){
-char str[] = "Module successfully loaded";
+char str[] = "Module successfully loaded\n";
 serial_write(str,sizeof(str));
 
 typedef void (*call_module_t)(void);
@@ -33,7 +35,7 @@ start_program();
 
 }
 else{
-char str[] = "Multiple modules loaded";
+char str[] = "Multiple modules loaded\n";
 serial_write(str,sizeof(str));
 }
 
